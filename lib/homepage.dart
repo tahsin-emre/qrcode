f68a0a246442a2f321202db4b0431c3016ff9782
Tahsin Emre <telli@tahsinemre.com>
2021-09-28 23:14:29 +0300

    swift support added

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String barcode = 'Unknown';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,5 +23,30 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
         title: const Text('Barkod Okuyucu ve Oluşturucu'),
       );
-  get buildBody => const Center();
+
+  get buildBody => Center(
+        child: ElevatedButton(
+          onPressed: () => scanBarcode(),
+          child: const Text('Oku'),
+        ),
+      );
+
+  Future<void> scanBarcode() async {
+    try {
+      final barcode = await FlutterBarcodeScanner.scanBarcode(
+        '#ff6666',
+        'İptal',
+        true,
+        ScanMode.BARCODE,
+      );
+
+      if (!mounted) return;
+
+      setState(() {
+        this.barcode = barcode;
+      });
+    } on PlatformException {
+      barcode = 'Failed to get platform version.';
+    }
+  }
 }
