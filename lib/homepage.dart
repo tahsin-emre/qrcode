a5f60939737148730304c7d78f38c4d5f5ddc05d
Tahsin Emre <telli@tahsinemre.com>
2021-10-06 23:39:11 +0300

    tabs

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:qrcode/create_barcode.dart';
+import 'package:qrcode/read_barcode.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,8 +9,15 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  String barcode = 'Unknown';
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  TabController? tabController;
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 2, vsync: this, initialIndex: 0);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,39 +26,25 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  get buildAppBar => AppBar(
+  AppBar get buildAppBar => AppBar(
         centerTitle: true,
         title: const Text('Barkod Okuyucu ve Oluşturucu'),
-      );
-
-  get buildBody => Column(
-        children: [
-          Text(barcode),
-          Center(
-            child: ElevatedButton(
-              onPressed: () => scanBarcode(),
-              child: const Text('Oku'),
+        bottom: TabBar(
+          controller: tabController,
+          tabs: const [
+            Tab(
+              icon: Icon(Icons.photo_camera_rounded),
+              text: 'Barkod Oku',
             ),
-          ),
-        ],
+            Tab(
+              icon: Icon(Icons.qr_code),
+              text: 'Barkod Oluştur',
+            )
+          ],
+        ),
       );
 
-  Future<void> scanBarcode() async {
-    try {
-      final barcode = await FlutterBarcodeScanner.scanBarcode(
-        '#ff6666',
-        'İptal',
-        true,
-        ScanMode.BARCODE,
-      );
-
-      if (!mounted) return;
-
-      setState(() {
-        this.barcode = barcode;
-      });
-    } on PlatformException {
-      barcode = 'Failed to get platform version.';
-    }
-  }
+  Widget get buildBody => TabBarView(
+      controller: tabController,
+      children: const [ReadBarcode(), CreateBarcode()]);
 }
--- lib/read_barcode.dart
+++ lib/read_barcode.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ReadBarcode extends StatefulWidget {
   const ReadBarcode({Key? key}) : super(key: key);
@@ -8,8 +10,35 @@ class ReadBarcode extends StatefulWidget {
 }
 
 class _ReadBarcodeState extends State<ReadBarcode> {
+  String barcode = '';
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [button, barcodeText],
+    );
+  }
+
+  Widget get button =>
+      ElevatedButton(onPressed: () => scanBarcode(), child: const Text('Oku'));
+
+  Widget get barcodeText => Text(barcode);
+
+  Future<void> scanBarcode() async {
+    try {
+      final barcode = await FlutterBarcodeScanner.scanBarcode(
+        '#ff6666',
+        'İptal',
+        true,
+        ScanMode.BARCODE,
+      );
+
+      if (!mounted) return;
+
+      setState(() {
+        this.barcode = barcode;
+      });
+    } on PlatformException {
+      barcode = 'Failed to get platform version.';
+    }
   }
 }
